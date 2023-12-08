b3c164adc70c555d313ebb040d2ee41ba2a99a9d
harimohan <harimohan.info@gmail.com>
2023-12-08 14:03:49 +0530

    bmi calculator

---
+++ src/component/BMI.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+const BMICalculator = () => {
+  const [weight, setWeight] = useState('');
+  const [height, setHeight] = useState('');
+  const [bmi, setBMI] = useState(null);
+
+  const calculateBMI = () => {
+    if (weight && height) {
+      const heightInMeters = height / 100;
+      const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+      setBMI(bmiValue);
+    } else {
+      setBMI(null);
+    }
+  };
+
+  return (
+    <div>
+      <h2>BMI Calculator</h2>
+      <div>
+        <label>
+          Weight (kg):
+          <input
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Height (cm):
+          <input
+            type="number"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
+          />
+        </label>
+      </div>
+      <button onClick={calculateBMI}>Calculate BMI</button>
+      {bmi !== null && (
+        <div>
+          <h3>Your BMI is: {bmi}</h3>
+          <p>Interpretation: {interpretBMI(bmi)}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const interpretBMI = (bmi) => {
+  if (bmi < 18.5) {
+    return 'Underweight';
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return 'Normal weight';
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
+  }
+};
+
+export default BMICalculator;
